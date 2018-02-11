6e26f759836f60643a58bdf2564970860cb94ff9
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2018-02-11 11:02:10 +0000

    removed browser animations from module imports

--- animate.module.ts
+++ animate.module.ts
@@ -1,15 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AnimateComponent} from './animate.component';
 
 @NgModule({
-    imports: [
-        CommonModule, BrowserAnimationsModule
-    ],
-    declarations: [AnimateComponent],
-    exports: [AnimateComponent]
+  imports: [
+    CommonModule
+  ],
+  declarations: [AnimateComponent],
+  exports: [AnimateComponent]
 })
 export class AnimateModule {
 }
